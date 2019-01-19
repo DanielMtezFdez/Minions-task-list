64e4594b24cde65463497c81bcef871d7c8d5fc6
daniel <dani.martinez.3.fernandez@gmail.com>
2019-01-19 21:45:51 +0100

    Adición de la activity Main y funcionalidades a los botones del Login

--- app/src/main/java/com/khezu/listatareas/Login.java
+++ app/src/main/java/com/khezu/listatareas/Login.java
@@ -1,9 +1,13 @@
 package com.khezu.listatareas;
 
+import android.content.Intent;
 import android.graphics.Typeface;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
 
@@ -20,5 +24,25 @@ public class Login extends AppCompatActivity {
         Typeface miFuente = Typeface.createFromAsset(getAssets(), "Mignone.ttf");
 
         titulo.setTypeface(miFuente);
+    }
+
+    public void crearCuenta(View view){
+        Toast toast = Toast.makeText(this, "Funcionalidad no disponible", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void accederApp(View view){
+        //User: itt
+        //pw: 123
+        TextInputEditText user = (TextInputEditText) findViewById(R.id.user);
+        TextInputEditText pw = (TextInputEditText) findViewById(R.id.pw);
+
+        if(user.getText().toString().toLowerCase().equals("itt") && pw.getText().toString().equals("123")){
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        } else {
+            Toast toast = Toast.makeText(this, "Usuario o contraseña no válidos", Toast.LENGTH_SHORT);
+            toast.show();
+        }
     }
 }
