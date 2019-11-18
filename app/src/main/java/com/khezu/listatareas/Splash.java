3930d463e26b22591dfd47ab5d5b29ff6213e8b5
DanielMtezFdez <dani.martinez.3.fernandez@gmail.com>
2019-11-18 21:24:44 +0100

    nueva imagen y configuracion de splash

--- app/src/main/java/com/khezu/listatareas/Splash.java
+++ app/src/main/java/com/khezu/listatareas/Splash.java
@@ -1,17 +1,13 @@
 package com.khezu.listatareas;
 
 import android.content.Intent;
-import android.graphics.Typeface;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.TextView;
 
-import org.w3c.dom.Text;
+public class Splash extends AppCompatActivity {
 
-public class Splash extends AppCompatActivity  implements Animation.AnimationListener {
-
+    private final int DURACION_SPLASH = 1000; // 1 segundo
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,41 +15,14 @@ public class Splash extends AppCompatActivity  implements Animation.AnimationLis
         //Ocultamos el ActionBar en el Splash
         getSupportActionBar().hide();
 
-        //Creamos fuente
-        Typeface miFuente = Typeface.createFromAsset(getAssets(), "Mignone.ttf");
-
-        TextView titulo = (TextView)findViewById(R.id.tituloSplash);
-        TextView cargando = (TextView)findViewById(R.id.cargando);
-
-        //Creamos animacion
-        Animation anim = AnimationUtils.loadAnimation(this, R.anim.anim_splash);
-        Animation anim2 = AnimationUtils.loadAnimation(this, R.anim.anim_splash_2);
-
-        //Establecemos fuente y animacion
-        titulo.setTypeface(miFuente);
-        cargando.setTypeface(miFuente);
-
-        titulo.setAnimation(anim);
-        cargando.setAnimation(anim2);
-
-        anim2.setAnimationListener(this);
-
-    }
-
-    @Override
-    public void onAnimationStart(Animation animation) {
-
-    }
-
-    @Override
-    public void onAnimationEnd(Animation animation) {
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        finish();
-    }
-
-    @Override
-    public void onAnimationRepeat(Animation animation) {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, DURACION_SPLASH);
 
     }
 }
